4a6dc32b3870543156eae925af0865a8b4a03589
Dmitri <test@test.com>
2019-05-18 01:50:54 -0300

    Primer commit

---
+++ consoleApp_001/Exercises.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleApp_001
+{
+    class Ejercicios
+    {
+
+        public void Operacion()
+        {
+
+        }
+    }
+}
---
+++ consoleApp_001/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace consoleApp_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables:
+            int num1;
+            int num2;
+            int sum;
+            int result;
+
+
+            string linea1;
+            string linea2;
+            Ejercicios ejercicio = new Ejercicios;
+
+            //Start
+            Console.WriteLine("Enter number to compare: ");
+            linea1 = Console.ReadLine();
+            num1 = int.Parse(linea1);
+
+            Console.WriteLine("Enter another number to compare: ");
+            linea2 = Console.ReadLine();
+            num2 = int.Parse(linea2);
+
+            if (num1 > num2)
+            {
+                sum = num1 + num2;
+                result = sum * 3;
+
+                Console.WriteLine("the result is: " + result);
+                Console.ReadKey();
+
+            }
+
+            else
+            {
+                Console.Clear();
+                Console.WriteLine(" system ERROR !!! ");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
